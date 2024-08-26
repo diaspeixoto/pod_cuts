5f6942b233da69784d19f2c2fe96ca1fd50ae423
eduardodias <eduardodias884@gmail.com>
2024-08-26 16:10:52 -0300

    Update the code

--- download_cut.py
+++ download_cut.py
@@ -3,24 +3,34 @@ import re
 import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import yt_dlp
+import subprocess
 
 def sanitize_filename(filename):
-    # Substitui espaços por underscores e remove caracteres especiais
     filename = re.sub(r'[^\w\s-]', '', filename).strip().replace(' ', '_')
     return filename
 
 def download_video(url):
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
-        'outtmpl': 'downloaded_video.%(ext)s',  # Save as .mp4
-        'merge_output_format': 'mp4',  # Ensure the output is in mp4 format
+        'outtmpl': '/home/eduardo/pod_cuts/downloads/downloaded_video.%(ext)s',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    return 'downloaded_video.mp4'
+
+    original_video = '/home/eduardo/pod_cuts/downloads/downloaded_video.webm'
+    converted_video = '/home/eduardo/pod_cuts/downloads/downloaded_video.mp4'
+
+    # Transcode the audio to AAC to avoid the experimental feature error
+    subprocess.run([
+        'ffmpeg', '-i', original_video,
+        '-c:v', 'copy',  # Copy video stream without re-encoding
+        '-c:a', 'aac',  # Transcode audio to AAC
+        converted_video
+    ])
+    
+    return converted_video
 
 def cut_video(video_path, cuts_csv):
-    # Create the 'cuts' folder if it doesn't exist
     if not os.path.exists('cuts'):
         os.makedirs('cuts')
 
@@ -29,7 +39,7 @@ def cut_video(video_path, cuts_csv):
         for row in reader:
             start_time = convert_to_seconds(row[0])
             end_time = convert_to_seconds(row[1])
-            output_filename = os.path.join('cuts', sanitize_filename(row[2]) + '.mp4')  # Save in 'cuts' folder
+            output_filename = os.path.join('cuts', sanitize_filename(row[2]))
             ffmpeg_extract_subclip(video_path, start_time, end_time, targetname=output_filename)
 
 def convert_to_seconds(time_str):
@@ -41,13 +51,11 @@ def convert_to_seconds(time_str):
         hours, minutes, seconds = parts
         return hours * 3600 + minutes * 60 + seconds
 
-
 if __name__ == "__main__":
     video_url = "https://www.youtube.com/watch?v=f7xRgss2IOQ&t=1105s"
     cuts_csv = "cortes_flow392_clovis.csv"
 
     video_path = download_video(video_url)
-    #video_path = "downloaded_video.mp4"
     cut_video(video_path, cuts_csv)
 
     print("Processo concluído!")
